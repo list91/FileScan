a462a707bf751d821096c89c4e885f91096a97e3
Stas <s_anufriev@list.ru>
2023-05-19 09:00:40 +0700

    перезапись структуры

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
+import os
+from PyQt5.QtWidgets import QApplication, QProgressBar, QWidget, QLabel
 
+def calculate_directory_size(directory_path, label):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(directory_path):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            total_size += os.path.getsize(filepath)
+            label.setText("Total size: " + str(round(total_size/(1024*1024*1024), 2)) + " GB) ||" + str(round(total_size/(1024*1024), 2)) + " MB, || "
+                          + str(total_size) + " - bytes || ")
+            QApplication.processEvents()
+    return total_size
 
-class ProgressBar(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle("File System Change Scanner")
-        self.progress = QtWidgets.QProgressBar()
-
-        self.start_button = QtWidgets.QPushButton("Start")
-        self.start_button.clicked.connect(self.start_progressbar)
-
-        self.stop_button = QtWidgets.QPushButton("Stop")
-        self.stop_button.clicked.connect(self.stop_progressbar)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.progress)
-        layout.addWidget(self.start_button)
-        layout.addWidget(self.stop_button)
-        self.setLayout(layout)
-
-    def start_progressbar(self):
-        self.progress.setRange(0, 100)
-        self.progress.setValue(0)
-
-        self.timer = QtCore.QBasicTimer()
-        self.timer.start(100, self)
-
-    def timerEvent(self, event):
-        if self.progress.value() >= 100:
-            self.timer.stop()
-            return
-
-        value = self.progress.value() + 1
-        self.progress.setValue(value)
-
-    def stop_progressbar(self):
-        if hasattr(self, 'timer'):
-            self.timer.stop()
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    widget = ProgressBar()
-    widget.show()
+def main():
+    app = QApplication([])
+    window = QWidget()
+    progress_bar = QProgressBar(window)
+    progress_bar.setGeometry(30, 40, 200, 25)
+    directory_path = "D:\Steam"#"D:/MAIN"
+    progress_bar.setMaximum(100)
+    progress_bar.setMinimum(0)
+    current_size = 0
+    label = QLabel(window)
+    label.setGeometry(30, 70, 300, 25)
+    window.show()
+    total_size = calculate_directory_size(directory_path, label)
+    for dirpath, dirnames, filenames in os.walk(directory_path):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            label.setText(filepath)
+            # process file here
+            current_size += os.path.getsize(filepath)
+            progress_percent = int((current_size / total_size) * 100)
+            progress_bar.setValue(progress_percent)
+            QApplication.processEvents()
     app.exec_()
+
+main()
